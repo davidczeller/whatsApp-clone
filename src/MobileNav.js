a8a54fac84aeeedc969728aa694e3a079a1eb1a5
David Czeller <david.czeller@cloudcode.hu>
2020-11-21 22:48:21 +0100

    fixes

--- src/MobileNav.js
+++ src/MobileNav.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 
-import { IconButton } from '@material-ui/core'
+import { IconButton, Popover } from '@material-ui/core'
+import { Settings, Person, RateReview } from "@material-ui/icons";
+
 
 import './MobileNav.css'
 
@@ -16,7 +18,8 @@ export function MobileNav(props) {
 
 
   let color = 'ffffff'
-  let activeColor = '9fd0c6'
+  // let activeColor = '9fd0c6'
+  let activeColor = 'ffffff'
 
   // if (active) {
   //   active.style.color = '#000000'
@@ -26,28 +29,59 @@ export function MobileNav(props) {
   //   color = '#000000'
   // }
 
+  const [anchorEl, setAnchorEl] = useState(false);
+
+  const id = anchorEl ? 'simple-popover' : undefined;
+
   return (
     <div className="iconContainer" >
-      <IconButton onClick={(e) => (setActive(e.currentTarget.querySelector('p')))}>
-        <img src={`https://img.icons8.com/wired/24/${color}/connection-status-on.png`} />
-        <p>Status</p>
+      <IconButton onClick={(e, idx) => console.log(e.currentTarget, idx)}>
+        <img src={`https://img.icons8.com/wired/24/${active ? activeColor : color}/connection-status-on.png`} />
+        <p style={{ color: active ? `#${activeColor}` : `#${color}` }} >Status</p>
       </IconButton>
       <IconButton onClick={(e) => (setActive(e.currentTarget.querySelector('p')))}>
-        <img src={`https://img.icons8.com/wired/24/${color}/phone.png`} />
-        <p>Calls</p>
+        <img src={`https://img.icons8.com/wired/24/${active ? activeColor : color}/phone.png`} />
+        <p style={{ color: active ? `#${activeColor}` : `#${color}` }}>Calls</p>
       </IconButton>
       <IconButton onClick={(e) => (setActive(e.currentTarget.querySelector('p')))}>
-        <img src={`https://img.icons8.com/dotty/24/${color}/camera.png`} />
-        <p>Camera</p>
+        <img src={`https://img.icons8.com/dotty/24/${active ? activeColor : color}/camera.png`} />
+        <p style={{ color: active ? `#${activeColor}` : `#${color}` }}>Camera</p>
       </IconButton>
       <IconButton onClick={(e) => (setOpen(false), setActive(e.currentTarget.querySelector('p')))}>
-        <img src={`https://img.icons8.com/dotty/24/${activeColor}/chat.png`} />
-        <p style={{ color: '#9fd0c6' }}>Chat</p>
+        <img src={`https://img.icons8.com/dotty/24/${active ? activeColor : color}/chat.png`} />
+        <p style={{ color: active ? `#${activeColor}` : `#${color}` }}>Chat</p>
       </IconButton>
-      <IconButton onClick={(e) => (setActive(e.currentTarget.querySelector('p')))}>
-        <img src={`https://img.icons8.com/dotty/24/${color}/settings.png`} />
-        <p>Settings</p>
+      <IconButton aria-describedby={id} onClick={(e) => setAnchorEl(e.currentTarget)}>
+        <img src={`https://img.icons8.com/dotty/24/${active ? activeColor : color}/settings.png`} />
+        <p style={{ color: active ? `#${activeColor}` : `#${color}` }}>Settings</p>
       </IconButton>
+      <Popover
+        id={id}
+        open={anchorEl}
+        anchorEl={anchorEl}
+        onClose={() => setAnchorEl(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+      >
+        <IconButton className='popoverButton'>
+          <RateReview />
+          New Room
+                </IconButton>
+        <IconButton className='popoverButton'>
+          <Person />
+          Profile
+                </IconButton>
+        <IconButton className='popoverButton'>
+          <Settings />
+          Settings
+                </IconButton>
+      </Popover>
     </div>
   )
 }
